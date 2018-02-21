0f1cc0f2f86bb14b590c92577489e4a8b3473168
Jack <mappinjack@users.noreply.github.com>
2018-02-21 15:44:07 -0500

    Clean up code and add download function

--- flaskr/flaskr.py
+++ flaskr/flaskr.py
@@ -1,8 +1,9 @@
 
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
 from watson_developer_cloud import ConversationV1
 from keys import apiKeys
+from dataDict import dataDict
 #import apiKeys
 
 app = Flask(__name__)
@@ -15,37 +16,43 @@ def home():
 
 @app.route('/askWatson', methods=['POST', 'GET'])
 def askWatson():
-	user_input = request.args.get('jsdata')
 	global context
+	user_input = request.args.get('jsdata')
+
 	conversation = ConversationV1(
 	username= apiKeys["username"],
 	password= apiKeys["password"],
 	version= apiKeys["version"])
 
 	workspace_id = apiKeys["workspace_id"]
-	context = {}
 
+	#print("Context in: " + str(context))
 	response = conversation.message(
 		workspace_id = workspace_id,
 		input = {
 		'text': user_input
 		},
 		context = context)
-
-	# If an intent was detected, print it to the console.
-	if response['intents']:
-		print('Detected intent: #' + response['intents'][0]['intent'])
+	
+	# Context is used to maintain the conversation between each API call
+	context = response['context']
+	# Output is a dict with a variety of keys, notable 'intents', 'text', 'action'
+	output = response['output']
+	# List of Watson identified intents
+	intents = response['intents'] if response['intents'] else None
+	# List of Watson identified entities
+	entities = response['entities'] if response['entities'] else None
 
 	# Print the output from dialog, if any.
-	if response['output']['text']:
-		processed_text = response['output']['text'][0]
-	
-	# Update the stored context with the latest received from the dialog.
-	context = response['context']
-	print(str(context))
-	print('Detected entities: ' + str(response['entities']))
-	#print 'Intents: ' + str(response['intents'])
-	#print response
-	# Prompt for next round of input.
-	# processed_text = text.upper()
-	return '<p>'+processed_text+'</p>' 
+	moreInfo = None
+	outputText = output['text'][0] if output['text'] else "No text returned"
+	action = output['action'] if 'action' in output else None
+
+	if action == 'download':
+		dataset = output['dataset']
+		dlFormat = output['format']
+		moreInfo = dataDict[dataset]['downloadUrls'][dlFormat]
+
+	# This will return the response.html template with variables
+	return render_template('response.html', outputText = outputText, action = action, moreInfo = moreInfo)
+
