be05dcc346f2d1591034982e9013e2dff1ac7bb6
malcolmlin-kr <36239468+malcolmlin-kr@users.noreply.github.com>
2018-03-09 15:16:07 -0500

    Website
    added website styling to the build and trying to get the chat functinality to work.
    What doesnt work: pulling map and downloading
    Still need to set up chatbox template and styling

---
+++ opendatabot/keys.py
@@ -0,0 +1,5 @@
+
+apiKeys = {"username":"248765ba-d7af-4059-9125-f2a67768df36",
+		   "password":"SFqNVQ0WgQhV",
+		   "version":"2017-04-21",
+		   "workspace_id":"13556271-e257-43fd-ac28-30b219307c24"}
